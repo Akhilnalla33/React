5325f1659014ba44bd7d755a1764ea6f4980a131
Akhilnalla33 <akhilchowdary_nalla@srmap.edu.in>
2025-08-01 20:23:31 +0530

    untilProps

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx';
 import Food from './Food.jsx';
 import Cards from './Cards.jsx';
+import Button from './Button.jsx';
+import Student from './Student.jsx';
 
 function App(){
 
@@ -10,10 +12,27 @@ function App(){
       {/* <Header/>
       <Food/>
       <Footer/> */}
+      {/* <Cards/>
       <Cards/>
       <Cards/>
-      <Cards/>
-      <Cards/>
+      <Cards/> */}
+
+      {/* <Button /> */}
+
+
+        {/* props = readonly properties that are shared b/w components 
+            A parent component can send data to a child component.
+             <Component key=value>*/}
+      <Student name = "Akhil" age ={20} isStudent={true}/>
+      <Student name ="Nikhil" age ={15} isStudent={false}/>
+      <Student name ="Bobu" age ={75} isStudent={false}/>
+      <Student name ="mamayaaaaa" age ={50} isStudent={true}/>
+      <Student />
+      
+
+
+
+
 
     </>
     
